4cc6154e3f4d83b6ff32c907aa4e2058972fc01d
cheneyshreve <cheneyshreve@users.noreply.github.com>
2017-12-15 15:47:35 -0800

    updated submit form to clear after entry

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     return (
       <div className="App">
        <h1>Bloc Chat</h1>
-       <RoomList name={this.props.name}/>
+       <RoomList/>
       </div>
     );
   }
